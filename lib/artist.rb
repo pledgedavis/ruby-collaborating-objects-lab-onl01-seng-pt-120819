295badac130aa3c41650c75bd9ea7367d1b6b031
Pledge Davis <pledgedavis14@gmail.com>
2020-01-24 00:01:56 +0000

    Automatically backed up by Learn

---
+++ lib/artist.rb
@@ -0,0 +1,83 @@
+require 'pry'
+class Artist 
+  attr_accessor :name, :artist
+  @@all = []
+  def initialize(name)
+       @name = name
+      # @@all << self
+      @artist = artist
+        @@all << self
+  end
+  
+  def self.all
+    @@all
+  end
+  
+   def songs
+      
+      
+  # artist.all      
+      Song.all.select {|song| song.artist == self}
+       
+   end  
+  
+  def add_song(new_song) 
+    # binding.pry
+      # songs.push(new_song)
+      # self.songs << new_song
+      new_song.artist = self
+#       This instance method receives an instance of a `Song` and associates it belongs to
+# _this_ `Artist` instance.
+
+  end
+  
+  def self.create(name)
+       self.new(name).tap {|artist| artist.save }
+      # research tap method
+  end
+
+  def save
+  @@all << self
+  
+  end
+ 
+  def self.find_or_create_by_name(name)
+  # string = name
+  #   research find method 
+  # @@all.find do |artist| 
+  #   if artist.name == name
+  #     return name
+  #   else
+  #     Artist.create(name)
+  #     # use create method to create artist
+  #   end
+  
+  if  @@all.find {|artist| artist.name == name } 
+    return @@all.find {|artist| artist.name == name }
+      
+      else
+        Artist.create(name)
+    
+   end
+  end
+  
+  def print_songs 
+  # songs
+     
+    # self.songs.select {|song| song.name}
+     
+    # self.songs.each {|song| song.name}
+    
+    self.songs.each {|song| puts song.name}
+  end
+end
+
+
+
+# This instance methods outputs the names of all songs associated wiwith this
+# `Artist` instance.
+
+
+
+
+
---
+++ lib/song.rb
@@ -0,0 +1,31 @@
+require 'pry'
+class Song 
+  attr_accessor :name, :artist
+  @@all = []
+  def initialize(name)
+  @name = name
+  # @@all << self
+  @artist = artist
+    @@all << self
+  end
+  
+  def self.all
+   
+   @@all
+# binding.pry
+  end
+  
+    def self.new_by_filename(new_song)
+      
+      
+    end
+  
+  def artist_name=(name)
+    self.artist = Artist.find_or_create_by_name(name)
+    artist.add_song(self)
+  end
+  
+ 
+  
+  
+end
