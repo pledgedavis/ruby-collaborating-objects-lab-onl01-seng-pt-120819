975532519051ed7223a7f135903530b95d36b442
Pledge Davis <pledgedavis14@gmail.com>
2020-01-24 00:02:28 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -21,8 +21,7 @@ class Song
     end
   
   def artist_name=(name)
-    self.artist = Artist.find_or_create_by_name(name)
-    artist.add_song(self)
+
   end
   
  
@@ -45,7 +44,8 @@ end
 
 
 
-
+    self.artist = Artist.find_or_create_by_name(name)
+    artist.add_song(self)
 
 
 
